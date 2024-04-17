2db49f12a3d28cbc9b17be522d18d6918ec4a850
Mwan3kii <agathawanja303@gmail.com>
2024-04-17 21:21:09 +0300

    place

--- models/city.py
+++ models/city.py
@@ -11,6 +11,7 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
     state = relationship("State", back_populates="cities")
+    places = relationship('Place', backref='cities', cascade='all, delete')
 
     def __init__(self, *args, **kwargs):
         """Initialization method for City"""
