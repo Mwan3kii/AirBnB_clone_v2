a812ea7e34108f3782c3b1982ff3d1bbf3713b5a
Mwan3kii <agathawanja303@gmail.com>
2024-04-18 21:03:48 +0300

    updated file storage

--- models/state.py
+++ models/state.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """ State class inherits from BaseModel and Base"""
-    name = Column(String(128), nullable=False)
-    __tablename__ = 'states'
-    cities = relationship("City", cascade="all, delete", backref="state")
-
-    def __init__(self, *args, **kwargs):
-        """Initialization method for State"""
-        super().__init__(*args, **kwargs)
+class State(BaseModel):
+    """ State class """
+    name = ""
