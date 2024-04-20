f2ccdc37ad5d97e21173644ee648579c7d60cde6
Mwan3kii <agathawanja303@gmail.com>
2024-04-20 22:25:03 +0300

    tests

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import unittest
 from console import HBNBCommand
 from models import storage
@@ -16,18 +17,6 @@ from unittest.mock import patch
 
 class TestConsole(unittest.TestCase):
     """Test cases for the command in the console."""
-    def test_missing_class_name(self):
-        test_output = HBNBCommand().onecmd("create")
-        self.assertIn("** class name missing **", test_output)
-
-    def test_non_existent_class_name(self):
-        """test"""
-        test_output = HBNBCommand().onecmd("create SomeClass")
-        self.assertIn("** class doesn't exist **", test_output)
-
-    def test_valid_class_name_with_parameters(self):
-        test_output = HBNBCommand().onecmd("create State name=\"California\"")
-        self.assertTrue(test_output.startswith("(hbnb)"))
 
     @unittest.skipIf(
             os.getenv('HBNB_TYPE_STORAGE') == 'db', 'FileStorage test')
