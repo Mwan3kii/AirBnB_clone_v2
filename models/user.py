ebf111f60f95f876c80a8ea5de0054d42444f8b1
Mwan3kii <agathawanja303@gmail.com>
2024-04-20 22:48:44 +0300

    doc style

--- models/user.py
+++ models/user.py
@@ -7,7 +7,17 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This class defines a user by various attributes
+
+    Attributes:
+        __tablename__ (str): Name of the MySQL table to store users.
+        email: (sqlalchemy String): The user email address.
+        password (sqlalchemy String): The user password.
+        first_name (sqlalchemy String): The user first name.
+        last_name (sqlalchemy String): The user last name.
+        places (sqlalchemy relationship): The Place relationship.
+        reviews (sqlalchemy relationship): The Review relationship.
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
