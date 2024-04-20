046548c13ca3c61401f8e527226554b53d9cf414
Mwan3kii <agathawanja303@gmail.com>
2024-04-20 21:32:26 +0300

    update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -9,6 +9,6 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """Represents an amenity data set."""
     __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                    viewonly=False)
