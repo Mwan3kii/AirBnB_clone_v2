fff2a7ce8bef6b68cb5d873d742fb2e7bdb21c0d
Mwan3kii <agathawanja303@gmail.com>
2024-04-20 21:29:15 +0300

    update place

--- models/place.py
+++ models/place.py
@@ -9,6 +9,15 @@ import models
 from os import getenv
 
 
+associated_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
+
+
 class Place(BaseModel, Base):
     """ Defines place class"""
     __tablename__ = "places"
