0f0c849d219f26c037ed48aae3f7a0227def3661
Mwan3kii <agathawanja303@gmail.com>
2024-05-04 21:52:48 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 """Fabric script thats distributes archive web servers"""
+from fabric.api import local
+from datetime import datetime
+import os
 from fabric.api import *
 import os
 env.hosts = ["35.174.176.33", "54.152.129.84"]
 env.user = 'ubuntu'
+
+
+def do_pack():
+    """Returns path to the created archive"""
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    stamp_name = f"versions/web_static_{timestamp}.tgz"
+    result = local(f"tar -cvzf {stamp_name} web_static")
+    if result.failed:
+        return None
+    return stamp_name
 
 
 def do_deploy(archive_path):
