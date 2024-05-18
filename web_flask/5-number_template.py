5536bb1122889ea3e92e4e66fd38955dea2fc0bb
Mwan3kii <agathawanja303@gmail.com>
2024-05-18 21:55:17 +0300

    html number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application listening on 0.0.0.0 port 5000"""
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -32,7 +32,13 @@ def python_text(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def num_n(n):
     """Display if n is an interger"""
-    return f"{n} is number"
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def html_n(n):
+    """Displays html page if n is interger"""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
