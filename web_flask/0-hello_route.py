49d44af8134156e6fe90e6bc3ed6718be7fb957d
Mwan3kii <agathawanja303@gmail.com>
2024-05-18 20:58:47 +0300

    documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# Starts a Flask web application listening on 0.0.0.0 port 5000
+"""Starts a Flask web application listening on 0.0.0.0 port 5000"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Displays hello hbnb on route /"""
     return "Hello HBNB!"
 
 
