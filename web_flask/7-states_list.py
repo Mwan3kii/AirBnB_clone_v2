56fe9f64605d9309ac03a344abc217aab4223869
Mwan3kii <agathawanja303@gmail.com>
2024-05-19 21:58:01 +0300

    list states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+"""starts web application must be listening on 0.0.0.0, port 5000"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
+from models import *
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays html page listing state id and state name"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
